873eaea7163be2009c24a14815becfc8d552a4dc
AustinHuang <449290878@qq.com>
2023-12-25 07:44:45 +0000

    add testpart

--- demo_baseline_onetest.py
+++ demo_baseline_onetest.py
@@ -7,7 +7,9 @@ import torch
 from torchvision import transforms
 from model import gaze_network
 import warp_norm
-
+import pandas as pd
+import utils
+import time
 
 trans = transforms.Compose([
         transforms.ToPILImage(),
@@ -16,20 +18,48 @@ trans = transforms.Compose([
                              std=[0.229, 0.224, 0.225]),
     ])
 
+cam_chen = './testpart/camChen.xml'  # this is camera calibration information file obtained with OpenCV
+fs_chen = cv2.FileStorage(cam_chen, cv2.FILE_STORAGE_READ)
+w_chen = 1300
+h_chen = 700
+camera_matrix_chen = fs_chen.getNode('Camera_Matrix').mat() # camera calibration information is used for data normalization
+camera_distortion_chen = fs_chen.getNode('Distortion_Coefficients').mat()
+cam_tan = './testpart/camTan.xml'  # this is camera calibration information file obtained with OpenCV
+fs_tan = cv2.FileStorage(cam_tan, cv2.FILE_STORAGE_READ)
+w_tan = 1920
+h_tan = 1080
+camera_matrix_tan = fs_tan.getNode('Camera_Matrix').mat() # camera calibration information is used for data normalization
+camera_distortion_tan = fs_tan.getNode('Distortion_Coefficients').mat()
+
 if __name__ == '__main__':
-    img_file_name = '/home/hgh/hghData/Datasets/Photo/118.jpg'
-    print('load input face image: ', img_file_name)
-    image = cv2.imread(img_file_name)
-    cam_tan = '/home/hgh/hghData/Datasets/camTan.xml'  # this is camera calibration information file obtained with OpenCV
-    fs_tan = cv2.FileStorage(cam_tan, cv2.FILE_STORAGE_READ)
-    camera_matrix_tan = fs_tan.getNode('Camera_Matrix').mat() # camera calibration information is used for data normalization
-    camera_distortion_tan = fs_tan.getNode('Distortion_Coefficients').mat()     
-    img_normalized, gcn = warp_norm.GazeNormalization(image, camera_matrix_tan, camera_distortion_tan,w = 1920, h = 1080, gc = np.array([1261,518]))
-    # img_file_name = '/home/hgh/hghData/Datasets/preprocessed_images/preprocessed_image_1.jpg'
-    # img_normalized = cv2.imread(img_file_name)
+    image_path = './testpart/1.jpg'
+    csv_file_path = './testpart/coordinate_test.csv'
+    df = pd.read_csv(csv_file_path, index_col='ID')
+    image_ID,_ = os.path.splitext(os.path.basename(image_path))
+    target_row = df.loc[int(image_ID)]
+    # print(target_row)
+    print('load input face image: ', image_ID)
+
+    start_time = time.time()
+
+    image = cv2.imread(image_path)
+
+
+    if target_row['subject'] == 'Tan':
+        camera_matrix = camera_matrix_tan
+        camera_distortion = camera_distortion_tan
+        w = w_tan
+        h = h_tan
+    else:
+        camera_matrix = camera_matrix_chen
+        camera_distortion = camera_distortion_chen
+        w = w_chen
+        h = h_chen
+    gc = np.array([int(target_row['x']),int(target_row['y'])])
+    img_normalized, gcn = warp_norm.GazeNormalization(image, camera_matrix, camera_distortion,gc, w, h)
     print('load gaze estimator')
     model = gaze_network()
-    model.cuda() # comment this line out if you are not using GPU
+    model.cuda()
     pre_trained_model_path = './ckpt/epoch_24_ckpt.pth.tar'
     if not os.path.isfile(pre_trained_model_path):
         print('the pre-trained gaze estimation model does not exist.')
@@ -46,12 +76,35 @@ if __name__ == '__main__':
     pred_gaze = model(input_var)  # get the output gaze direction, this is 2D output as pitch and raw rotation
     pred_gaze = pred_gaze[0] # here we assume there is only one face inside the image, then the first one is the prediction
     pred_gaze_np = pred_gaze.cpu().data.numpy()  # convert the pytorch tensor to numpy array
-
+    end_time = time.time()
+    elapsed_time = end_time - start_time
+    print(f"Elapsed Time: {elapsed_time} seconds")
     print('prepare the output')
-    # draw the facial landmarks
-    print(pred_gaze_np)
+    print('Predict normalization gaze vector(pitch yaw):', pred_gaze_np)
+    print('True normalization gaze vector(pitch yaw):', warp_norm.vector_to_pitchyaw(np.array([gcn]))[0])
+    e = utils.angular_error(np.array([pred_gaze_np]),warp_norm.vector_to_pitchyaw(np.array([gcn])))[0]
+    print('error:', e)
+
+
+    print('true vector', gcn)
+    print('pred vector', warp_norm.pitchyaw_to_vector(np.array([pred_gaze_np])))
+
+
+
+    # print('Normalization true gaze point:', warp_norm.vector_to_gc(gcn.reshape((1,3))[0],w,h))
+    # print('Normalization pred gaze point:', warp_norm.vector_to_gc(pred_gaze_np,w,h))
+    
+
+    # save the normalization image
     face_patch_gaze = warp_norm.draw_gaze(img_normalized, pred_gaze_np)  # draw gaze direction on the normalized face image
     face_patch_gaze = warp_norm.draw_gaze(img_normalized, gcn, color=(0,255,0))  # draw gaze direction on the normalized face image
     output_path = './test/results_gaze.jpg'
     print('save output image to: ', output_path)
     cv2.imwrite(output_path, face_patch_gaze)
+    
+    # compare the gaze point
+    print('Original Gaze Point:', gc)
+    pred_gaze = warp_norm.pitchyaw_to_vector(np.array([pred_gaze_np]))[0]
+    pred_gaze = np.dot(np.linalg.inv(R),pred_gaze)
+    pred_gaze_point = warp_norm.vector_to_gc(pred_gaze,w,h)
+    print('Predict Gaze Point:', pred_gaze_point)
