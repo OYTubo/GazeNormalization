63726b8c725fd19a8e403f666bfe9d4a409cbb93
AustinHuang <449290878@qq.com>
2024-05-07 08:56:24 +0000

    modified:   demo_video.py

--- demo_video.py
+++ demo_video.py
@@ -15,37 +15,54 @@ from torchvision import transforms
 import pickle
 import gaze_normalize
 import os
+import math
+from collections import deque
 
-fileroot = '/home/hgh/hghData/gaze/eve/valpart/val01/step030_video_Wikimedia-Joy-and-Heron-Animated-CGI-Spot-by-Passion-Pictures/webcam_c.h5'
+cam_drozy = r"D:\DROZY_and_NTHU\GazeNormalization-cpu_1\testpart\DROZY\kinect-intrinsics.yaml"  #drozy的相机参数
+fs_drozy = cv2.FileStorage(cam_drozy, cv2.FILE_STORAGE_READ)
+camera_matrix_drozy = fs_drozy.getNode('intrinsics').mat()
+k, p = fs_drozy.getNode('k').mat(), fs_drozy.getNode('p').mat()
+camera_distortion_drozy = np.zeros((5,1))
+for i in range(3):
+    camera_distortion_drozy[i]=k[i]
+for j in range(2):
+    camera_distortion_drozy[j+3]=p[j]
+w_drozy = 512
+h_drozy = 424
+fs_drozy.release()
 
-file = h5py.File(fileroot, 'r')
-file.keys()
 
-camera_matrix = file['camera_matrix'][:]
-# print('camera_matrix:')
-# print(camera_matrix)
-camera_transformation = file['camera_transformation'][:]
-# print('camera_transformation:')
-# print(camera_transformation)
-head_rvec = file['head_rvec']['data']
-# print('head_rvec:')
-# print(head_rvec)
-head_tvec = file['head_tvec']['data']
-# print('head_tvec:')
-# print(head_tvec)
-tobii = file['face_PoG_tobii']['data']
-# print('gaze[0]:')
-# print(tobii[0])
-gaze_o = file['face_o']['data']
-# print('gaze_o[0]:')
-# print(gaze_o[0])
-gazen = file['face_g_tobii']['data']
-# print('gazen[0]:')
-# print(gazen[0])
-facial_landmarks = file['facial_landmarks']['data']
-# print('facial_landmarks:')
-# print(facial_landmarks)
-pixel_scale = file['millimeters_per_pixel'][:]
+# fileroot = '/home/hgh/hghData/gaze/eve/valpart/val01/step030_video_Wikimedia-Joy-and-Heron-Animated-CGI-Spot-by-Passion-Pictures/webcam_c.h5'
+#
+# file = h5py.File(fileroot, 'r')
+# file.keys()
+#
+camera_matrix=camera_matrix_drozy
+# camera_matrix = file['camera_matrix'][:]
+# # print('camera_matrix:')
+# # print(camera_matrix)
+# camera_transformation = file['camera_transformation'][:]
+# # print('camera_transformation:')
+# # print(camera_transformation)
+# head_rvec = file['head_rvec']['data']
+# # print('head_rvec:')
+# # print(head_rvec)
+# head_tvec = file['head_tvec']['data']
+# # print('head_tvec:')
+# # print(head_tvec)
+# tobii = file['face_PoG_tobii']['data']
+# # print('gaze[0]:')
+# # print(tobii[0])
+# gaze_o = file['face_o']['data']
+# # print('gaze_o[0]:')
+# # print(gaze_o[0])
+# gazen = file['face_g_tobii']['data']
+# # print('gazen[0]:')
+# # print(gazen[0])
+# facial_landmarks = file['facial_landmarks']['data']
+# # print('facial_landmarks:')
+# # print(facial_landmarks)
+# pixel_scale = file['millimeters_per_pixel'][:]
 # print(pixel_scale)
 # 模型读取
 trans = transforms.Compose([
@@ -56,26 +73,152 @@ trans = transforms.Compose([
     ])
 model = gaze_network()
 model.cuda()
-pre_trained_model_path = './ckpt/epoch_24_ckpt.pth.tar'
+pre_trained_model_path = r"D:\DROZY_and_NTHU\GazeNormalization-cpu_1\ckpt\epoch_24_ckpt.pth.tar"
 ckpt = torch.load(pre_trained_model_path)
 model.load_state_dict(ckpt['model_state'], strict=True)
 model.eval()
 
+def popHead(lst):
+    if len(lst)>=1800:
+        lst.pop(0)
+    return lst
+
+def returnAlert(blinkin60s,frameCountReturn,nextBegin):
+    alert=0
+    if frameCountReturn > 5:
+        blinkin60s = popHead(blinkin60s)
+        blinkin60s.append(2)
+        for j in range(nextBegin - i):
+            blinkin60s = popHead(blinkin60s)
+            blinkin60s.append(0)
+    else:
+        blinkin60s = popHead(blinkin60s)
+        blinkin60s.append(2)
+        for j in range(nextBegin - i):
+            blinkin60s = popHead(blinkin60s)
+            blinkin60s.append(0)
+    if frameCountReturn > 10:
+        alert=1
+    return alert,blinkin60s
+
+def baselineBn(dataY):
+    lenX = len(dataY)
+    dataY_b = []
+    dataY_b.append(dataY[0])
+    Alpha = [0] * lenX
+    for i in range(1, lenX):
+        Alpha[i] = smoothingFactor(dataY_b, i)
+        bI = (1 - Alpha[i]) * dataY_b[i - 1] + Alpha[i] * dataY[i]
+        #print(f'bI{bI}')
+        dataY_b.append(round(bI,4))
+    #print(f'bn:{dataY_b}')
+    return dataY_b
+
+
+def smoothingFactor(dataY_b, n):
+    a0 = 0.4
+    ad = 15
+    aa = 0.5
+    ab = 2
+    am = 0.7
+    exp1 = (-1) * ad * ((dataY[n] - dataY[n - 1]) ** 2)
+    exp_1 = math.exp(exp1)
+    exp2 = (-1) * aa * (dataY[n] - dataY_b[n - 1]) if dataY[n] - dataY_b[n - 1] > 0 else 0
+    exp_2 = math.exp(exp2)
+    exp3 = (-1) * ab * (dataY_b[n - 1] - dataY[n]) if dataY_b[n - 1] - dataY[n] > 0 else 0
+    exp_3 = math.exp(exp3)
+    exp4 = dataY[n] - am * getMedian(dataY, n)
+    exp_4 = 1 if exp4 >= 0 else 0
+    return a0 * exp_1 * exp_2 * exp_3 * exp_4
+
+
+def getMedian(dataY, n):
+    d = []
+    d.extend(dataY[1:n+1])
+    d.sort()
+    if len(d) % 2 == 0:
+        return (d[len(d) // 2 - 1] + d[len(d) // 2]) / 2
+    else:
+        return d[len(d) // 2]
+
+def blinkCounter(data_normalized):
+    i=0
+    # flag=0
+    blinkin60s=[]
+    for k in range(1800):
+        blinkin60s.append(0)
+    alert=0
+    frameCount=0
+    blinkCount=0
+    blinkLonggest=0
+    while i<len(data_normalized):
+        if(data_normalized[i]<0.65):
+            flag=i
+            frameCountReturn,nextBegin=frameBackandForth(data_normalized,flag)
+            frameCount+=frameCountReturn
+            alert,blinkin60s=returnAlert(blinkin60s,frameCountReturn,nextBegin)
+            i=nextBegin
+            blinkCount+=1
+            if frameCountReturn>blinkLonggest:
+                blinkLonggest=frameCountReturn
+        i+=1
+        blinkin60s.append(0)
+    c1=0
+    c2=0
+    for x in blinkin60s:
+        if x==1:
+            c1+=1
+        elif x==2:
+            c2+=1
+    if c1>0 and c2>0:
+        if c2/(c1+c2)>0.25:
+            alert=1
+    return frameCount,blinkCount,blinkLonggest,alert
+
+def frameBackandForth(data_normalized,flag):
+    frameCount=0
+    nextBegin=flag
+    for i in range(1,(flag+1 if flag<len(data_normalized)-flag else len(data_normalized)-flag)):
+        if(data_normalized[flag-i]<=0.75):
+            frameCount+=1
+        if(data_normalized[flag+i]<=0.75):
+            frameCount+=1
+            nextBegin+=1
+        if(data_normalized[flag-i]>0.75 and data_normalized[flag+i]>0.75):
+            break
+    return frameCount,nextBegin
+
 # 数据预处理
-video_path = '/home/hgh/hghData/test20240306.mkv'
+# video_path = '/home/hgh/hghData/test20240306.mkv'
+video_path="./testpart/3-1.mp4"
 cap = cv2.VideoCapture(video_path)
 res = []
 images = []
 idx = 0
 ret = True
-camera_distortion = np.array([-0.16321888, 0.66783406, -0.00121854, -0.00303158, -1.02159927])
+# camera_distortion = np.array([-0.16321888, 0.66783406, -0.00121854, -0.00303158, -1.02159927])
+camera_distortion=camera_distortion_drozy
 preds = gaze_normalize.xmodel()
-while True:
+counter=0
+dataX=[]
+dataY=[]
+dataY_b=[]
+while counter<300:
+    dataX.append(counter)
+    counter+=1
+    print(f'counter:{counter}')
     ret,image = cap.read()
+    # print(f'image:{image}')
     if ret == False:
         break
-    gaze_normalize_eve = gaze_normalize.GazeNormalize(image,(0,0), camera_matrix,camera_distortion,preds,is_video=True)
-    image_warp = gaze_normalize_eve.norm()
+    gaze_normalize_eve = gaze_normalize.GazeNormalize(image,(0,0), camera_matrix,camera_distortion,preds,is_video=True,image=image) ##True to False
+    image_warp, real_eyelip_distance = gaze_normalize_eve.norm()
+    dataY.append(real_eyelip_distance)
+    dataY_b=baselineBn(dataY)
+    data_normalized = []
+    for i in range(len(dataY)):
+        opening = dataY[i] / dataY_b[i]
+        data_normalized.append(round(opening, 4))
     if gaze_normalize_eve.err:
         text = 'EAR: No Detected Face'
         font = cv2.FONT_HERSHEY_SIMPLEX
@@ -88,14 +231,19 @@ while True:
         image_draw = image
     else:
         gaze_normalize_eve.pred('./ckpt/epoch_24_ckpt.pth.tar', image_warp)
-        image_draw = gaze_normalize_eve.draw_gaze()
+        frameCount, blinkCount, blinkLonggest,alert=blinkCounter(data_normalized)
+        image_draw = gaze_normalize_eve.draw_gaze(frameCount=frameCount, blinkCount=blinkCount, blinkLonggest=blinkLonggest,alert=alert,data_normalized=data_normalized)
+        print(f'{data_normalized}')
+        # image_draw=gaze_normalize_eve.draw_norm_gaze()
     # res.append(gaze_normalize_eve)
     images.append(image_draw)
     idx += 1
 cap.release()
 
 height, width, layers = images[0].shape
-video = cv2.VideoWriter('/home/hgh/hghData/output_3_6.mp4', cv2.VideoWriter_fourcc(*'mp4v'), 25, (width, height))
+# video = cv2.VideoWriter('/home/hgh/hghData/output_3_6.mp4', cv2.VideoWriter_fourcc(*'mp4v'), 25, (width, height))
+video = cv2.VideoWriter("./test/output_3-1.mp4", cv2.VideoWriter_fourcc(*'mp4v'), 25, (width, height))
+
 
 # 将每张图片逐帧写入视频
 for image in images:
--- gaze_normalize.py
+++ gaze_normalize.py
@@ -43,20 +43,38 @@ class GazeNormalize():
         # detected_faces = self.predictor['dlib_detector'](cv2.cvtColor(self.image, cv2.COLOR_BGR2RGB), 1) ## convert BGR image to RGB for dlib
         # if len(detected_faces) == 0:
         #    print('warning: no detected face by dlib')
+
         if self.is_video == False:
             self.image_path = os.path.join(dataset_path, self.image_name)
             image = cv2.imread(self.image_path)
         else:
             image = self.image
-        preds = self.predictor['fa_detector'].get_landmarks(image)
-        if preds is None:
-            print('warning: no detected face by fa')
-            self.hr = np.zeros((1,3))
-            self.ht = np.zeros((1,3))
-            self.err = True
-            return 0
-        max_face_index = np.argmax(np.apply_along_axis(self.calculate_face_area, axis=1, arr=preds))
-        landmarks = preds[max_face_index]
+            # print(f'image{self.image}')
+
+        predictor = dlib.shape_predictor('./modules/shape_predictor_68_face_landmarks.dat')
+        face_detector = dlib.get_frontal_face_detector()
+        # face detection
+        detected_faces = face_detector(cv2.cvtColor(image, cv2.COLOR_BGR2RGB),1)  ## convert BGR image to RGB for dlib
+
+        shape = predictor(image, detected_faces[0])  ## only use the first detected face (assume that each input image only contains one face)
+        shape = face_utils.shape_to_np(shape)
+        landmarks = []
+        for (x, y) in shape:
+            landmarks.append((x, y))
+        landmarks = np.asarray(landmarks)
+
+        # preds = self.predictor['fa_detector'].get_landmarks_from_image(image)  #做了修改
+        # # preds = self.predictor[].get_landmarks_from_image(image)
+        # if preds is None:
+        #     print('warning: no detected face by fa')
+        #     self.hr = np.zeros((1,3))
+        #     self.ht = np.zeros((1,3))
+        #     self.err = True
+        #     return 0
+        # max_face_index = np.argmax(np.apply_along_axis(self.calculate_face_area, axis=1, arr=preds))
+        # landmarks = preds[max_face_index]
+
+
         # else:
         #     largest_face = max(detected_faces, key=lambda rect: rect.width() * rect.height())
         #     print("max face position:", largest_face)
@@ -68,6 +86,7 @@ class GazeNormalize():
         #     landmarks = np.asarray(landmarks)
         landmarks_sub = landmarks[[36, 39, 42, 45, 31, 35], :]
         landmarks_sub = landmarks_sub.astype(float)
+        self.landmarks=landmarks.astype(float)
         self.landmarks_sub = landmarks_sub.reshape(6, 1, 2)
 
         # Get Ear
@@ -76,19 +95,46 @@ class GazeNormalize():
             Ear.append((np.linalg.norm(landmarks[41+6*i]-landmarks[37+6*i],2) + np.linalg.norm(landmarks[40+6*i]-landmarks[38+6*i],2))/(2*np.linalg.norm(landmarks[36+6*i]-landmarks[39+6*i],2)))
         self.Ear = np.mean(np.asarray(Ear))
 
+        print('ear:{}'.format(self.Ear))
+
         # 2.Estimate Head Pose
         face_model_load = np.loadtxt('./modules/face_model.txt')  # Generic face model with 3D facial landmarks
         landmark_use = [20, 23, 26, 29, 15, 19]  # we use eye corners and nose conners
         face_model = face_model_load[landmark_use, :]
+        landmarks_eye_corner = [20, 23, 26, 29]
+        face_model_eye_corner = face_model_load[landmarks_eye_corner, :]
         facePts = face_model.reshape((6, 1, 3))
         hr,ht = self.estimateHeadPose(facePts)
         self.hr = hr.reshape((1,3))
         self.ht = ht.reshape((1,3))
 
         # 3.Normalize Image
-        self.warp_image = self.xtrans(face_model)
+        self.warp_image, Fc, Fc_lip = self.xtrans(face_model,face_model_eye_corner)
 
-        return self.warp_image
+
+
+
+
+        #Get lip distance and eye corner distance
+        lip_distance = []
+        for i in range(2):
+            lip_distance.append(((np.linalg.norm(landmarks[41 + 6 * i] - landmarks[37 + 6 * i], 2) + np.linalg.norm(
+                landmarks[40 + 6 * i] - landmarks[38 + 6 * i], 2)) / 2))
+        lip_distance = np.mean(np.array(lip_distance))
+        landmarks_eye_corner = []
+        for i in range(2):
+            landmarks_eye_corner.append(np.linalg.norm(landmarks[36 + 6 * i] - landmarks[39 + 6 * i]))
+        landmarks_eye_corner = np.mean(np.array(landmarks_eye_corner))
+
+        distance_baseline = (np.linalg.norm(Fc_lip[:, 0] - Fc_lip[:, 1], ord=2, axis=0) + np.linalg.norm(Fc_lip[:, 2] - Fc_lip[:, 3],
+                                                                                                 ord=2, axis=0)) / 2
+        transfer_facter = distance_baseline / landmarks_eye_corner
+        real_eyelip_distance = lip_distance * transfer_facter
+
+
+
+
+        return self.warp_image,real_eyelip_distance
     
     def calculate_face_area(self, face):
         min_x, max_x = np.min(face[0]), np.max(face[0])
@@ -103,7 +149,7 @@ class GazeNormalize():
             ret, rvec, tvec = cv2.solvePnP(face_model, self.landmarks_sub, self.camera_matrix, self.camera_distortion, rvec, tvec, True)
         return rvec, tvec
 
-    def xtrans(self, face_model):
+    def xtrans(self, face_model,face_model_lip):
         # normalized camera parameters
         focal_norm = 960
         distance_norm = 600  # normalized distance between face and camera
@@ -113,6 +159,7 @@ class GazeNormalize():
         hr = self.hr.reshape((3,1))
         hR = cv2.Rodrigues(hr)[0]  # rotation matrix, [3,3]
         Fc = np.dot(hR, face_model.T) + ht # [3,50]
+        Fc_lip=np.dot(hR, face_model_lip.T)
         self.face_center = np.mean(Fc,axis=1).reshape((3, 1))
         # normalize image
         distance = np.linalg.norm(self.face_center)  # actual distance between and original camera
@@ -143,23 +190,31 @@ class GazeNormalize():
         else:
             image = cv2.imread(self.image_path)
         img_warped = cv2.warpPerspective(image, self.W, roiSize)  # image normalization
-        return img_warped
-    
-    def pred(self, model, img_warped):
+        return img_warped, Fc ,Fc_lip
+
+
+    def pred(self, model_in, img_warped):
         '''This method is used for real-time video gaze direction prediction'''
-        if model == None:
+        if model_in == None:
             print('load gaze estimator')
             model = gaze_network()
-            model.cuda()
+            # model.cuda()
             pre_trained_model_path = './ckpt/epoch_24_ckpt.pth.tar'
             if not os.path.isfile(pre_trained_model_path):
                 print('the pre-trained gaze estimation model does not exist.')
                 exit(0)
             else:
                 print('load the pre-trained model: ', pre_trained_model_path)
-            ckpt = torch.load(pre_trained_model_path)
+            ckpt = torch.load(pre_trained_model_path,map_location=torch.device('cpu'))
             model.load_state_dict(ckpt['model_state'], strict=True)  # load the pre-trained model
             model.eval()  # change it to the evaluation mode
+        #quan na dao xia mian
+        model = gaze_network()
+        pre_trained_model_path = './ckpt/epoch_24_ckpt.pth.tar'
+        ckpt = torch.load(pre_trained_model_path, map_location=torch.device('cpu'))
+        model.load_state_dict(ckpt['model_state'], strict=True)  # load the pre-trained model
+        model.eval()  # change it to the evaluation mode
+        ##dao zhe li wei zhi
         input_var = img_warped[:, :, [2, 1, 0]]  # from BGR to RGB
         trans = transforms.Compose([
                 transforms.ToPILImage(),
@@ -168,9 +223,11 @@ class GazeNormalize():
                             std=[0.229, 0.224, 0.225]),
                 ])
         input_var = trans(input_var)
-        input_var = torch.autograd.Variable(input_var.float().cuda())
+        # input_var = torch.autograd.Variable(input_var.float().cuda())
+        input_var = torch.autograd.Variable(input_var.float())
         input_var = input_var.view(1, input_var.size(0), input_var.size(1), input_var.size(2))  # the input must be 4-dimension
-        pred_gaze = model(input_var)  # get the output gaze direction, this is 2D output as pitch and raw rotation
+        model_1=model
+        pred_gaze = model_1(input_var)  # get the output gaze direction, this is 2D output as pitch and raw rotation
         self.pred_gaze = pred_gaze[0].cpu().data.numpy() # here we assume there is only one face inside the image, then the first one is the prediction
         return self.pred_gaze
     
@@ -221,7 +278,7 @@ class GazeNormalize():
 
         # 绘制点和线
         cv2.circle(image_out, (int(x+pos), int(-y+pos)), radius=5, color=(0, 0, 255), thickness=-1)  # 红色点
-        cv2.circle(image_out, (int(0+pos), int(0+pos)), radius=5, color=(255, 0, 0), thickness=-1)  # 蓝色点
+        cv2.circle(image_out, (int(0+pos), int(0+pos)), radius=1, color=(255, 0, 0), thickness=-1)  # 蓝色点
         cv2.line(image_out, (pos, pos), (int(x+pos), int(-y+pos)), color=(0, 0, 0), lineType = cv2.LINE_AA)  # 黑色虚线
 
         # 绘制圆形投影（根据x_circle, y_circle的值绘制）
@@ -260,7 +317,7 @@ class GazeNormalize():
         return image_out
 
 
-    def draw_gaze(self, thickness = 2, color=(0,0,255)):
+    def draw_gaze(self, thickness = 2, color=(0,0,255), frameCount=0, blinkCount=0, blinkLonggest=0,alert=0,data_normalized=[]):
         '''Draw gaze angle on given image with a given eye positions.'''
         # 1.Reduction
         gaze_vector = warp_norm.pitchyaw_to_vector(self.pred_gaze.reshape((1,2)))
@@ -281,17 +338,32 @@ class GazeNormalize():
                     thickness, cv2.LINE_AA, tipLength=0.2)
         
         # 3.Draw landmarks&EAR
-        for landmark in self.landmarks_sub.reshape((6,2)):
-            cv2.circle(image_out, tuple(np.round(landmark).astype(int)), 5, (255,0,0), -1)
+        # for landmark in self.landmarks_sub.reshape((6,2)):
+        #     cv2.circle(image_out, tuple(np.round(landmark).astype(int)), 1, (255,0,0), -1)
+        for landmark in self.landmarks.reshape((68, 2)):
+              cv2.circle(image_out, tuple(np.round(landmark).astype(int)), 1, (255,0,0), -1)
         # add EAR
+        # text = 'EAR:{:2f}'.format(self.Ear)
         text = 'EAR:{:2f}'.format(self.Ear)
         font = cv2.FONT_HERSHEY_SIMPLEX
-        font_scale = 1
-        font_thickness = 2
+        font_scale = 0.4
+        font_thickness = 1
         font_color = (255, 255, 255)  # 白色
         text_size = cv2.getTextSize(text, font, font_scale, font_thickness)[0]
-        text_position = (image_out.shape[1] - text_size[0] - 10, image_out.shape[0] - 10)
+        text_position = (image_out.shape[1] - text_size[0] - 10, image_out.shape[0] -20)
         cv2.putText(image_out, text, text_position, font, font_scale, font_color, font_thickness)
+        text2= 'blinkCount:{:2f}'.format(blinkCount)
+        text_position2 = (image_out.shape[1] - text_size[0] - 10, image_out.shape[0] - 30 )
+        cv2.putText(image_out, text2, text_position2, font, font_scale, font_color, font_thickness)
+        text3= 'frameCount:{:2f}'.format(frameCount)
+        text_position3 = (image_out.shape[1] - text_size[0] - 10, image_out.shape[0] - 40)
+        cv2.putText(image_out, text3, text_position3, font, font_scale, font_color, font_thickness)
+        text4= 'blinkLonggest:{:2f}'.format(blinkLonggest)
+        text_position4 = (image_out.shape[1] - text_size[0] - 10, image_out.shape[0] - 50)
+        cv2.putText(image_out, text4, text_position4, font, font_scale, font_color, font_thickness)
+        text5= 'alert:{}'.format(alert)
+        text_position5 = (image_out.shape[1] - text_size[0] - 10, image_out.shape[0] - 60)
+        cv2.putText(image_out, text5, text_position5, font, font_scale, font_color, font_thickness)
         return image_out
     
     def vector_to_screen(self, pixel_scale):
